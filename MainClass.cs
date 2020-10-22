fc5cc11f4e34ec83b33a1e80ea9bf0724721cdd7
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-22 12:28:47 +0530

    [Kalyan] added die rolling method to get number between 1 to 6

--- MainClass.cs
+++ MainClass.cs
@@ -14,6 +14,15 @@ namespace SnakeAndLadder
         {
             ///Single player with start position at 0
             int player1 = 0;
+            Console.WriteLine(RollDice());
+        }
+
+        public int RollDice()
+        {
+            int n ;
+            Random random = new Random();
+            n = random.Next(7);
+            return (n == 0 ? 1 : n);
         }
     }
 }
