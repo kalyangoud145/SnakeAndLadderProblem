53b773819910347c8d7325974901abade048f8bb
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-22 13:31:08 +0530

    [Kalyan] Printing the number of times die rolled to win and its postion

--- MainClass.cs
+++ MainClass.cs
@@ -4,6 +4,7 @@ namespace SnakeAndLadder
 {
     class MainClass
     {
+        public const int WIN_POINT = 100;
         static void Main(string[] args)
         {
             MainClass start = new MainClass();
@@ -13,8 +14,31 @@ namespace SnakeAndLadder
         public void StartGame()
         {
             ///Single player with start position at 0
-            int player1 = 0;
+            int player1 = 0, diceValue = 0;
+            int player1DieCount = 0;
+            while (true)
+            {
+                diceValue = RollDice();
+                player1DieCount++;
+                player1 = CalculatePlayerPostion(player1, diceValue);
+                Console.WriteLine("Player position 1: " + player1);
+                if (player1 == WIN_POINT)
+                {
+                    Console.WriteLine("Player1 Won");
+                    Console.WriteLine("Die rolled " + player1DieCount + " times to win");
+                    break;
+                }
+            }
+           
 
+        }
+
+        public int RollDice()
+        {
+            int n;
+            Random random = new Random();
+            n = random.Next(7);
+            return (n == 0 ? 1 : n);
         }
 
         public int CalculatePlayerPostion(int player1, int diceValue)
@@ -41,7 +65,7 @@ namespace SnakeAndLadder
             {
                 player1 = 0;
             }
-            else if (player1 > 100)
+            else if (player1 > WIN_POINT)
             {
                 player1 = player1 - diceValue;
             }
